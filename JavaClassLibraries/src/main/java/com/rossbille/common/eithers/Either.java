4c279aea0226944480a668708a4b3dc90e8ace84
Ross Bille <rossjbille@gmail.com>
2014-04-25 23:38:48 +1000

    [TASK] removed Action and Function interfaces in favour of jdk8's java.util.function.Function&Consumer versions added implementation of a Both including a helper function to split a list of withers into a both of lists of lefts and rights

--- JavaClassLibraries/src/main/java/com/rossbille/common/eithers/Either.java
+++ JavaClassLibraries/src/main/java/com/rossbille/common/eithers/Either.java
@@ -1,23 +1,41 @@
 package com.rossbille.common.eithers;
 
-import com.rossbille.common.functions.Action;
-import com.rossbille.common.functions.Function;
-
+import java.util.function.Consumer;
+import java.util.function.Function;
+/**
+ * An Either is an object that can contain either a value of type TLeft or TRight but never both
+ * by convention an Either contains a right value if a function has executed successfully and left otherwise
+ * @author rossbille
+ * @param <TLeft>
+ * @param <TRight> 
+ */
 public class Either<TLeft,TRight>
 {
 	private final TLeft left;
 	private final TRight right;
  
+	/**
+	 * Constructs a left type Either
+	 * @param <TLeft> The left type of this Either
+	 * @param value the value this Either contains
+	 * @return the constructed Either
+	 */
 	public static <TLeft> Either left(TLeft value) {
 		if (value == null) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("expected non-null left value");
 		}
 		return new Either(value, null);
 	}
-
+	
+	/**
+	 * Constructs a right type Either
+	 * @param <TRight> The right type of this either
+	 * @param value the value this Either contains
+	 * @return the constructed either
+	 */
 	public static <TRight> Either right(TRight value) {
 		if (value == null) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("expected non-null left value");
 		}
 		return new Either(null, value);
 	}
@@ -31,27 +49,40 @@ public class Either<TLeft,TRight>
 		return left != null;
 	}
 	
-	
-	public void Case(Action<TLeft> ofLeft, Action<TRight> ofRight){
+	/**
+	 * Runs ofLeft if this either contains a left value or ofRight if it contains a right value
+	 * @throws IllegalArgumentException if either actions are null
+	 * @param ofLeft the action to run if this either contains a left value
+	 * @param ofRight the action to run if this either contains a right value
+	 */
+	public void Case(Consumer<TLeft> ofLeft, Consumer<TRight> ofRight){
 		if(ofLeft == null){
-			throw new IllegalArgumentException("ofLeft");
+			throw new IllegalArgumentException("expected non-null ofLeft action");
 		}
 		if(ofRight == null){
-			throw new IllegalArgumentException("ofRight");
+			throw new IllegalArgumentException("expected non-null ofRight action");
 		}
 		if(isLeft()){
-			ofLeft.invoke(left);
+			ofLeft.accept(left);
 		}else{
-			ofRight.invoke(right);
+			ofRight.accept(right);
 		}
 	}
 	
+	/**
+	 * Applies ofLeft if this either contains a left value or ofRight if it contains a right value
+	 * @throws IllegalArgumentException if either functions are null
+	 * @param <Out> the return type 
+	 * @param ofLeft the function to run if this either contains a left value
+	 * @param ofRight the function to run if this either contains a right value
+	 * @return The result of the function that is applied of type Out
+	 */
 	public<Out> Out Case(Function<TLeft,Out> ofLeft, Function<TRight,Out> ofRight){
 		if(ofLeft == null){
-			throw new IllegalArgumentException("ofLeft");
+			throw new IllegalArgumentException("expected non-null ofLeft function");
 		}
 		if(ofRight == null){
-			throw new IllegalArgumentException("ofRight");
+			throw new IllegalArgumentException("expected non-null ofRight function");
 		}
 		if(isLeft()){
 			return (Out) ofLeft.apply(left);
